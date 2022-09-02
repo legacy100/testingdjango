02057ee30405eaa4b26fd24fada8577c71f38f8e
shopapp <chikanwofor@gmail.com>
2022-09-02 12:20:37 +0100

    nc

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Post
+
 
 # Create your tests here.
+# To run a test in our terminal
+# py manage.py test
+# pip install coverage
+# coverage run manage.py test
+# coverage report
+# coverage html
+# coverage run --omit='*/env/*' manage.py test
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.blog = Post.objects.create(title='django', author='django', slug='django')
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'django')
